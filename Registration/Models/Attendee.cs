c3cd3cc851fc2e3a076f1aa6081127ffab6149c6
David Yen <david.yen@argodata.com>
2014-10-21 16:43:52 -0500

    added more form validation

--- Registration/Models/Attendee.cs
+++ Registration/Models/Attendee.cs
@@ -9,17 +9,20 @@ namespace Registration.Models
     public class Attendee
     {
         public int Id { get; set; }
-        [Required]
-        [Display(Name = "Name 姓名")]
+        [Required(ErrorMessage = "Name is required 請告訴我們您的大名")]
+        [Display(Name = "Name 姓名:")]
         public string Name { get; set; }
-        [Required]
-        [Display(Name = "Email 電子郵件地址")]
+        [Required(ErrorMessage = "Email is required 請告訴我們您的電子郵件以方便連絡")]
+        [Display(Name = "Email 電子郵件地址:")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address 請提供正確的電子郵件地址")]
         public string Email { get; set; }
-        [Display(Name = "Number of people 人數")]
+        [Range(1, 100)]
+        [Required(ErrorMessage = "Please provide the number of attendees 請告訴我們有多少位參加")]
+        [Display(Name = "Number of people 人數:")]
         public Int32 NumberOfAttendee { get; set; }
-        [Display(Name = "Would you like to join the banque? 是否參加晚宴?")]
+        [Display(Name = "Would you like to join the banque 是否參加晚宴?")]
         public bool AttendBanquet { get; set; }
-        [Display(Name = "Special Note: ")]
+        [Display(Name = "Special Note 特別需求: ")]
         public string Comment { get; set; }
     }
 }
