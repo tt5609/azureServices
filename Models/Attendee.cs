99a496f31785ee1ad8f3baa14e7fa1ddc2183f69
tt5609 <dy4716@gmail.com>
2014-10-20 23:49:31 -0500

    Add more files

---
+++ Models/Attendee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Attendee
+/// </summary>
+public class Attendee
+{
+	public Attendee()
+	{
+		public int Id { get; set; }
+    public string Name { get; set; }
+
+}
+}
