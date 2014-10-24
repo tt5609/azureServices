c26d2eb8da09a08fd8da773a1a187bf53356a809
David Yen <david.yen@argodata.com>
2014-10-24 16:55:16 -0500

    trying to make mobile friendly

--- Registration/Global.asax.cs
+++ Registration/Global.asax.cs
@@ -13,6 +13,7 @@ namespace Registration
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
